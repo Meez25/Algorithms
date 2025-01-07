a1085273ccd51781ba7f7796df153619af598176
Yann BRONNER <yann.bronner@gmail.com>
2025-01-07 21:43:57 +0100

    tabulation

---
+++ DynamicProgramming/CanSum_Tabulation/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	target := 300
+	numbers := []int{14, 7}
+	result := canSum(target, numbers)
+	fmt.Println(result, "in", time.Since(start))
+}
+
+func canSum(target int, numbers []int) bool {
+	table := make([]bool, target+1)
+	table[0] = true
+
+	for i, val := range table {
+		for _, number := range numbers {
+			if val == true && i+number < len(table) {
+				table[i+number] = true
+			}
+		}
+	}
+
+	return table[target]
+}
